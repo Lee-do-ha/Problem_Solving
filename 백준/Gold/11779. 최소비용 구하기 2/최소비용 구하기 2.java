dd9c6da64236f3ba84dfbf44435482465a411da3
Lee-do-ha <122585454+Lee-do-ha@users.noreply.github.com>
2023-03-10 00:04:36 +0900

    Update 최소비용 구하기 2.java

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -5,12 +5,14 @@ import java.util.*;
 
 public class Main {
 
+    // 각 정점당 최소비용 저장할 배열
     static int[] distnace;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
         PriorityQueue<int[]> PQ = new PriorityQueue<>(new Comparator<int[]>() {
+            // 최소비용으로 정렬하기 위한 override
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o1[1] - o2[1];
@@ -22,6 +24,7 @@ public class Main {
 
         distnace = new int[N+1];
         ArrayList<int[]>[] List = new ArrayList[N+1];
+        // 이전에 들린 노드 저장하기 위한 배열 -> 경로 찾기 위해서
         int[] preNode = new int[N+1];
 
 
@@ -42,6 +45,7 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
+        // 시작 정점의 비용 0, 이전 노드 0으로 초기화
         distnace[start] = 0;
         preNode[start] = 0;
         PQ.add(new int[] {start, distnace[start]});
@@ -50,9 +54,10 @@ public class Main {
             int[] A = PQ.poll();
             
             if(A[0] == end) break;
-
+                
             if(!List[A[0]].isEmpty()){
                 for(int i = 0 ; i < List[A[0]].size() ; i++){
+                    // 다음정점이 가지고 있는 최소 비용보다 현재 정점에서 다음 정점으로 가는 비용이 더 낮은 경우 갱신
                     if(distnace[A[0]] + List[A[0]].get(i)[1] < distnace[List[A[0]].get(i)[0]]){
                         distnace[List[A[0]].get(i)[0]] = distnace[A[0]] + List[A[0]].get(i)[1];
                         PQ.add(new int[] {List[A[0]].get(i)[0], distnace[List[A[0]].get(i)[0]]});
@@ -64,9 +69,11 @@ public class Main {
 
         int result = distnace[end];
 
+        // 지나온 정점 저장할 리스트
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(end);
 
+        // 시작 정점의 이전 노드는 0으로 저장했기 때문에 0이 나올때
         while(end != 0){
             arr.add(preNode[end]);
             end = preNode[end];
