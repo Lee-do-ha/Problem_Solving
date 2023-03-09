5004bfcaefe4d3fd70d1f5e8df0db6c5208c4da3
Lee-do-ha <122585454+Lee-do-ha@users.noreply.github.com>
2023-03-09 21:44:02 +0900

    Update 임계경로.java

--- 백준/Platinum/1948. 임계경로/임계경로.java
+++ 백준/Platinum/1948. 임계경로/임계경로.java
@@ -10,6 +10,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
+    // 도착 정점과 가중치 저장
 	static class node{
 		int department, weight;
 		public node(int department, int weight) {
@@ -19,6 +20,7 @@ public class Main {
 		}
 	}
 	
+    // 도착 정점 거리
 	static class departmentNode{
 		int distance;
 		public departmentNode(int distance) {
@@ -27,6 +29,7 @@ public class Main {
 		}
 	}
 	
+    // 출발 정점과 도착 정점 경로 저장
 	static class path{
 		int start, end;
 		public path(int start, int end) {
@@ -65,17 +68,23 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 		
+        // 방문체크 배열
 		visited = new boolean[N+1];
+        // 연결된 정점 저장할 연결리스트
 		List = new ArrayList[N+1];
+        // 역추적하기위한 연결리스트
 		backList = new ArrayList[N+1];
+        // 도착 정점 저장할 배열
 		int[] targetNode = new int[N+1];
 		departmentNode[] maxNode = new departmentNode[N+1];
 		
+        // 연결리스트들 초기화
 		for(int i = 1 ; i < N+1 ; i++){
 			List[i] = new ArrayList<>();
 			backList[i] = new ArrayList<>();
 		}
 		
+        
 		for(int i = 0 ; i < M ; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
@@ -86,10 +95,12 @@ public class Main {
 			targetNode[b]++;
 		}
 		
+        // 시작 정점과 도착 정점 입력 받기
 		st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
 		
+        // 시작 정점은 거리 0, 아닌 정점들은 최소값으로 초기화
 		for(int i = 1 ; i < N+1 ; i++) {
 			if(i == start) {
 				maxNode[i] = new departmentNode(0);
@@ -98,6 +109,7 @@ public class Main {
 			}
 		}
 		
+        // 큐에 시작 정점 인덱스 넣고 시작
 		queue = new LinkedList<Integer>();
 		queue.add(start);
 				
@@ -105,6 +117,8 @@ public class Main {
 			
 			int curNode = queue.poll();
 			
+            // 큐에 연결된 정점이 있는 경우 연결된 정점으로 가는 길이 더 가중치가 큰 경로가 되는 경우에만 
+            // 해당 정점 큐에 넣고 다시 탐색하면서 최장 경로 배열 갱신
 			if(!List[curNode].isEmpty()) {
 				for(int i = 0 ; i < List[curNode].size() ; i++) {
 					if(maxNode[curNode].distance + List[curNode].get(i).weight > maxNode[List[curNode].get(i).department].distance) {
@@ -117,14 +131,19 @@ public class Main {
 			}
 		}
 		
+        // 도착 정점의 최장 경로
 		System.out.println(maxNode[end].distance);
 		
 		int num = 0;
 		
+        // 역추적하기위해 도착 정점 큐에 넣고 시작
 		queue.add(end);
 		
+        // 이미 지나온 길 체크하기 위해 boolean 배열 사용
 		boolean[] visited = new boolean[N+1];
 		
+        // 도착 정점 넣고 역으로 올라가면서 연결된 정점까지의 최장 거리가 현재 정점에서의 가중치 뺀 값과 같으면
+        // 해당 정점이 최장 경로로 오는 경로이므로 큐에 넣고 갯수 추가
 		while(!queue.isEmpty()) {
 			
 			int curNode = queue.poll();
@@ -135,7 +154,6 @@ public class Main {
 						if(maxNode[curNode].distance - backList[curNode].get(i).weight == maxNode[backList[curNode].get(i).department].distance ) {
 							queue.add(backList[curNode].get(i).department);
 							num++;
-//							System.out.println(curNode + " " + backList[curNode].get(i).department);
 						}
 					}
 				}
@@ -145,15 +163,3 @@ public class Main {
 		System.out.println(num);
 	}
 }
-/*
-5
-7
-1 2 1
-1 3 3
-2 3 2
-2 4 1
-2 5 3
-3 5 1
-4 5 1
-1 5
-*/
