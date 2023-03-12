ca6ca44d561a5268130293c64e20dcb497c98bab
Lee-do-ha <122585454+Lee-do-ha@users.noreply.github.com>
2023-03-12 15:45:01 +0900

    Update ［모의 SW 역량테스트］ 보물상자 비밀번호.java

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -6,8 +6,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
-
+H
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,32 +23,34 @@ public class Solution {
 			
 			String str = br.readLine();
 			
+			// 문자열 중복을 해결하기 위해 HastSet에 문자열 저장
 			HashSet<String> hash = new HashSet<>();
 			
+			// 문자열 돌리기 위해 문자열 2개 더하기
 			String strsum = str+str;
 			
-//			System.out.println(strsum);
-			
+			// 문자열의 크기가 N이라 할때 N/4의 크기로 자르면서 HastSet에 저장
 			for(int k = 0 ; k < N/4 ; k++) {
 				for(int i = 0 ; i < N ; i += N/4) {
 					hash.add(strsum.substring(0+i+k, N/4+i+k));
 				}
 			}
 			
-//			System.out.println(hash);
-			
+			// 16진수를 10진수로 변환하기 위해 추가해줄 문자열
 			String ox = "0x";
 			
+			// 10진수로 바뀐 수 저장할 ArrayList
 			ArrayList<Long> List = new ArrayList<>();
 			
+			// 10진수로 변환후 ArrayList에 저장
 			for(String i : hash) {
 				List.add(Long.decode(ox+i));
 			}
 			
+			// 내림차순으로 정렬
 			Collections.sort(List, Collections.reverseOrder());
 			
-//			System.out.println(List);
-									
+			// M번째로 큰 숫자 출력
 			sb.append("#" + test + " " + List.get(M-1)).append("\n");
 		}
 		System.out.println(sb);
