df9898d7086608b8165773d75c667bb6dc4a89c7
Lee-do-ha <122585454+Lee-do-ha@users.noreply.github.com>
2023-03-12 19:40:03 +0900

    Update 전화번호 목록.java

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -19,8 +19,10 @@ public class Main {
             for(int i = 0 ; i < K ; i++){
                 str[i] = br.readLine();
             }
-
+            
+            // 문자열 오름차순 정렬 = 알파벳 순서
             Arrays.sort(str);
+            
             if(Stringcheck(K,str) == false){
                 sb.append("NO").append("\n");
             }else{
@@ -30,6 +32,8 @@ public class Main {
         System.out.println(sb);
     }
 
+    // 다음 문자열이 이번 문자열로 시작하는지 체크
+    // startWith 함수 사용
     private static boolean Stringcheck(int n, String[] s){
         for(int i = 0 ; i < s.length-1 ; i++){
             if(s[i+1].startsWith(s[i])){
