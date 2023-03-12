0db7c9c6019ba7dbe8d66d8a8d1748b347fe6dde
Lee-do-ha <122585454+Lee-do-ha@users.noreply.github.com>
2023-03-12 15:50:29 +0900

    Update 최소 스패닝 트리.java

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -6,6 +6,8 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	
+	// 출발 정점, 도착 정점, 가중치 저장할 클래스
+	// Kruskal's Algorithm 사용하려면 가중치 오름차순으로 정렬해야하므로 compareTo 사용
 	static class path implements Comparable<path>{
 		int start, end, weight;
 		public path(int start, int end, int weight) {
@@ -21,6 +23,7 @@ public class Solution {
 		}
 	}
 	
+	// 조상 저장할 배열
 	static int[] parents;
 
 	public static void main(String[] args) throws IOException {
@@ -51,11 +54,16 @@ public class Solution {
 				Path[i] = new path(a, b, c);
 			}
 			
+			// 가중치 순으로 정렬
 			Arrays.sort(Path);
 			
+			// 결과가 될 최소 비용
 			long result = 0 ;
+			
+			// 연결된 간선의 갯수
 			int count = 0;
 			
+			// 가중치가 낮은 것부터 시작 정점과 도착 정점이 연결되있는지 체크하고 안되어있으면 연결하고 연결된 간선 갯수 1개 플러스
 			for(int i = 0 ; i < M ; i++) {
 				if(findSet(Path[i].start) == findSet(Path[i].end)) {
 					continue;
@@ -64,6 +72,7 @@ public class Solution {
 					result += Path[i].weight;
 				}
 				
+				// 총 N개의 정점 연결해야하므로 N-1개의 간선이면 모두 연결됨
 				if(++count == N-1) break;
 			}
 			
@@ -72,12 +81,14 @@ public class Solution {
 		System.out.println(sb);
 	}
 	
+	// 조상 찾을 메소드
 	private static int findSet(int n) {
 		if(parents[n] == n) return n;
 		
 		return parents[n] = findSet(parents[n]);
 	}
 	
+	// 조상 통합할 
 	private static void union(int a, int b) {
 		if(findSet(a) == findSet(b)) {
 			return;
