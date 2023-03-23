cb362820ee493e7f3f4e468e0ba62b0e9687a515
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-03-23 19:24:01 +0900

    Update 벽 부수고 이동하기 4.java

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -23,6 +23,7 @@ public class Main {
 
         map = new int[N][M];
 
+        // 맵 입력받기
         for(int i = 0 ; i < N ; i++){
             String str = br.readLine();
             for(int j = 0 ; j < M ; j++){
@@ -30,8 +31,13 @@ public class Main {
             }
         }
 
+        // BFS진행할 큐
         queue = new LinkedList<>();
+        
+        // 전체 맵 BFS 돌릴 때 방문처리할 배열
         visited = new boolean[N][M];
+        
+        // 0이 아닌 지점 방문처리할 배열
         addvisited = new boolean[N][M];
         for(int i = 0 ; i < N ; i++){
             for(int j = 0 ; j < M ; j++){
@@ -51,21 +57,30 @@ public class Main {
     }
 
     private static void check(int x, int y){
+        // 더해줘야할 좌표 저장할 리스트
         addSet = new ArrayList<>();
+        // 현재 좌표 방문처리
         visited[x][y] = true;
         queue.add(new int[] {x,y});
+        // 0의 개수 
         int region = 1;
+        
+        // 4방 탐색 BFS
         while(!queue.isEmpty()){
             int[] k = queue.poll();
             for(int i = 0 ; i< 4 ; i++){
                 int changeX = k[0] + dx[i];
                 int changeY = k[1] + dy[i];
 
+                // 좌표가 범위를 벗어났는지 체크
                 if(changeX >= 0 && changeX < N && changeY >= 0 && changeY < M){
+                    
+                    // 좌표값이 0이고 아직 방문하지 않은 지점이라면 큐에 넣고 방문처리
                     if(map[changeX][changeY] == 0 && visited[changeX][changeY] == false){
                         queue.add(new int[] {changeX,changeY});
                         visited[changeX][changeY] = true;
                         region++;
+                    // 좌표값이 0이 아니고 이번 BFS에서 방문했는지 체크
                     }else if(map[changeX][changeY] != 0 && addvisited[changeX][changeY] == false){
                         addSet.add(new int[] {changeX,changeY});
                         addvisited[changeX][changeY] = true;
@@ -74,6 +89,9 @@ public class Main {
             }
         }
 
+        // 현재 값 추가해줘야 할 좌표들 값 추가해주고 방문하지않은것으로 체크
+        // 다음 BFS진행할때 또 더해줘야할수도 있기때문에 false
+        // 매번 배열을 만드는 경우에 시간초과가 나서 해당 BFS가 끝났을때 다시 원상복귀
         for(int[] a : addSet){
             map[a[0]][a[1]] += region;
             addvisited[a[0]][a[1]] = false;
