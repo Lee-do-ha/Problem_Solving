acc2248602b2be239a1a08e3954f832aeb39efbf
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-03-30 23:55:28 +0900

    Update 행성 터널.java

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -1,3 +1,5 @@
+// 모든 행성에 관해서 탐색하여 경로 저장할 경우 메모리 초과함
+// => 모든 행성의 거리는 x,y,z기준 가장 최소값이므로 각 x,y,z 기준으로 정렬하고 큐에 저장
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,6 +9,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    // 행성 번호와 좌표 저장할 클래스
     static class space{
         int index, x, y, z;
 
@@ -18,6 +21,8 @@ public class Main {
         }
     }
 
+    // 행성 번호들과 해당 경로의 길이 저장
+    // 경로의 길이순으로 정렬하기 위해 Comparable
     static class path implements Comparable<path>{
         int start, end, price;
 
@@ -33,6 +38,7 @@ public class Main {
         }
     }
 
+    // 조상 저장할 배열
     static int[] parents;
 
     public static void main(String[] args) throws IOException {
@@ -42,6 +48,7 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         parents = new int[N];
+        // 조상 본인으로 초기화
         for(int i = 0 ; i < N ; i++){
             parents[i] = i;
         }
@@ -53,40 +60,52 @@ public class Main {
             Spaces[i] = new space(i ,Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
         }
 
+        // x기준으로 정렬
         Arrays.sort(Spaces, ((o1, o2) -> o1.x - o2.x));
         for(int i = 0 ; i < N-1 ; i++){
             pq.add(new path(Spaces[i].index, Spaces[i+1].index, Math.min(Math.abs(Spaces[i].x - Spaces[i+1].x), Math.min(Math.abs(Spaces[i].y - Spaces[i+1].y), Math.abs(Spaces[i].z - Spaces[i+1].z)))));
         }
+        // y기준으로 정렬
         Arrays.sort(Spaces, ((o1, o2) -> o1.y - o2.y));
         for(int i = 0 ; i < N-1 ; i++){
             pq.add(new path(Spaces[i].index, Spaces[i+1].index, Math.min(Math.abs(Spaces[i].x - Spaces[i+1].x), Math.min(Math.abs(Spaces[i].y - Spaces[i+1].y), Math.abs(Spaces[i].z - Spaces[i+1].z)))));
         }
+        // z기준으로 정렬
         Arrays.sort(Spaces, ((o1, o2) -> o1.z - o2.z));
         for(int i = 0 ; i < N-1 ; i++){
             pq.add(new path(Spaces[i].index, Spaces[i+1].index, Math.min(Math.abs(Spaces[i].x - Spaces[i+1].x), Math.min(Math.abs(Spaces[i].y - Spaces[i+1].y), Math.abs(Spaces[i].z - Spaces[i+1].z)))));
         }
 
+        // 최종 값
         int result = 0;
+        // 몇개 연결했는지 세기 위함
         int n = 0;
+        
+        // 크루스칼 시작
         while(!pq.isEmpty()){
             path cur = pq.poll();
+            // 이미 이어져있다면 continue
             if(findSet(cur.start) == findSet(cur.end)) continue;
 
+            // 조상 결합하고 값 더해주고 연결된 경로수 더해주기
             unionSet(cur.start, cur.end);
             result += cur.price;
             n++;
+            // 총 N개이므로 N-1개의 경로가 이어지면 종료
             if(n == N-1) break;
         }
 
         System.out.println(result);
     }
 
+    // 조상 찾는 메소드
     private static int findSet(int a){
         if(a == parents[a]) return a;
 
         return parents[a] = findSet(parents[a]);
     }
 
+    // 조상 이어줄 메소드
     private static void unionSet(int a, int b){
         parents[findSet(b)] = findSet(a);
     }
