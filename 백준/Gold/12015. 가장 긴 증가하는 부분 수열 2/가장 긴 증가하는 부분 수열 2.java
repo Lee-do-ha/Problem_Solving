8da0b23140e2f521444bda54d9fb027a1fbf4cd4
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-03 00:19:52 +0900

    Update 가장 긴 증가하는 부분 수열 2.java

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -12,7 +12,10 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
+        // 주어진 입력 받을 배열
         int[] arr = new int[N];
+        
+        // 증가하는 수열 저장할 배열
         int[] length = new int[N+1];
 
         st = new StringTokenizer(br.readLine());
@@ -20,14 +23,18 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 초기값 세팅
         length[1] = arr[0];
 
+        // 초기길이 세팅
         int nowLength = 1;
 
         for(int i = 1 ; i < N ; i++) {
+            // 증가하는 수열의 맨끝값보다 크다면 길이 증가시키고 값 넣어주기
             if(arr[i] > length[nowLength]) {
                 length[nowLength+1] = arr[i];
                 nowLength++;
+            // 수열의 값보다 작다면 대체할 자리 찾기 -> 이분탐색
             }else{
                 int start = 1;
                 int end = nowLength;
@@ -40,16 +47,12 @@ public class Main {
                         end = mid;
                     }
                 }
+                // 증가하는 수열이므로 이전값과 같으면 안됨
                 if(length[start-1] != arr[i]){
                     length[start] = arr[i];
                 }
             }
-//        	for(int k = 1 ; k < nowLength+1 ; k++) {
-//        		System.out.print(length[k]+ " ");
-//        	}
-//        	System.out.println();
         }
-
         System.out.println(nowLength);
     }
 }
