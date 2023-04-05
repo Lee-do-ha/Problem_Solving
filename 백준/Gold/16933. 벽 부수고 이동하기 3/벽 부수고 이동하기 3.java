a8293e63a0374de2e4a360e1dc50aa43c4ebe2d6
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-05 23:15:55 +0900

    Update 벽 부수고 이동하기 3.java

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -35,6 +35,8 @@ public class Main {
         visited = new boolean[N][M][K+1];
 
         queue = new LinkedList<>();
+        
+        // x좌표, y좌표, 밤낮체크
         queue.add(new int[] {0, 0, 0});
         visited[0][0][0] = true;
 
@@ -42,6 +44,7 @@ public class Main {
         while(!queue.isEmpty()){
             int size = queue.size();
 
+            // 시간 재기위해 사이즈만큼만 돌리기
             for(int i = 0 ; i < size ; i++){
                 int[] cur = queue.poll();
                 if(cur[0] == N-1 && cur[1] == M-1){
@@ -54,13 +57,17 @@ public class Main {
                     int changeY = cur[1] + dy[k];
                     if(changeX < 0 || changeX >= N || changeY < 0 || changeY >= M) continue;
 
+                    // 해당 좌표가 0이면 밤낮 상관없이 이동 가능
                     if(map[changeX][changeY] == 0 && !visited[changeX][changeY][cur[2]]){
                         queue.add(new int[] {changeX, changeY, cur[2]});
                         visited[changeX][changeY][cur[2]] = true;
+                        // 해당 좌표가 1이면 밤낮 유무와 벽을 부술수 있는지 체크
                     }else if(map[changeX][changeY] == 1 && cur[2] < K && !visited[changeX][changeY][cur[2]+1]){
+                        // 현재가 낮이면 부수고 진행
                         if(time%2 == 0){
                             queue.add(new int[] {changeX, changeY, cur[2]+1});
                             visited[changeX][changeY][cur[2]+1] = true;
+                            // 현재가 밤이면 낮이 된 경우도 BFS진행
                         }else{
                             queue.add(new int[]{cur[0],cur[1],cur[2]});
                         }
@@ -69,6 +76,8 @@ public class Main {
             }
             time++;
         }
+        
+        // 0~K개의 벽을 부셔서 방문했다면 true로 체크
         boolean flag = false;
 
         for(int i = 0 ; i < K+1 ; i++){
@@ -78,8 +87,10 @@ public class Main {
             }
         }
 
+        // true이면 시간 출력
         if(flag){
             System.out.println(time);
+            // false라면 -1 출력
         }else{
             System.out.println(-1);
         }
