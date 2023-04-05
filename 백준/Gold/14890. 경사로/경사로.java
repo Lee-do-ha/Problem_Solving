7cf30673cf72f08958c6dd92e04209acae66c8ac
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-05 23:10:00 +0900

    Update 경사로.java

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, K;
 
+    // 좌표, 높이, 경사로가 설치되었는지 유무 체크
     static class Node{
         int index, height, used;
 
@@ -38,13 +39,13 @@ public class Main {
 
         for(int i = 0 ; i < N ; i++){
             
-            // 가로
+            // 가로 BFS체크
             boolean lowCheck = false;
             queue.add(new Node(0, map[i][0], 0));
 
             while(!queue.isEmpty()){
                 Node now = queue.poll();
-                // 마지막 좌표
+                // 마지막 좌표까지 도달하면 true로 체크
                 if(now.index == N-1){
                     lowCheck = true;
                     queue.clear();
@@ -56,33 +57,39 @@ public class Main {
                     queue.add(new Node(now.index+1, now.height, 0));
                 }
 
-                // 아래로 경사로
+                // 현재 기준으로 더 낮은쪽으로 경사로 체크
                 if(now.index + K < N){
                     if(now.height-1 == map[i][now.index+K]){
+                        // 모두 같은 높이인지 체크하기
                         boolean check = true;
+                        // 하나라도 다르면 false
                         for(int k = now.index+1 ; k < now.index+K ; k++){
                             if(map[i][now.index+K] != map[i][k]){
                                 check = false;
                                 break;
                             }
                         }
+                        // 모두 같다면 낮은쪽 좌표 경사로 설치된것으로 BFS진행
                         if(check){
                             queue.add(new Node(now.index+ K, now.height-1, 1));
                         }
                     }
                 }
 
-                // 위로 경사로
+                // 위로 경사로 지을려면 현재 좌표에 경사로가 안지어져있어야함
                 if(now.used != 1){
                     if(now.index + K < N){
                         if(now.height+1 == map[i][now.index+K]){
+                            // 모두 같은 높이인지 체크하기
                             boolean check = true;
+                            // 하나라도 다르면 false
                             for(int k = now.index+1 ; k < now.index+K ; k++){
                                 if(map[i][now.index] != map[i][k]){
                                     check = false;
                                     break;
                                 }
                             }
+                            // 모두 같다면 높은쪽 좌표로 BFS진행
                             if(check){
                                 queue.add(new Node(now.index+ K, now.height+1, 0));
                             }
@@ -95,25 +102,22 @@ public class Main {
 
 
 
-            // 세로
+            // 세로 -> 가로랑 같은 방식
             boolean curCheck = false;
             queue.add(new Node(0, map[0][i], 0));
 
             while(!queue.isEmpty()){
                 Node now = queue.poll();
-                // 마지막 좌표
                 if(now.index == N-1){
                     curCheck = true;
                     queue.clear();
                     break;
                 }
 
-                // 오른쪽 칸 높이 같을 때
                 if(map[now.index+1][i] == map[now.index][i]){
                     queue.add(new Node(now.index+1, now.height, 0));
                 }
 
-                // 아래로 경사로
                 if(now.index + K < N){
                     if(now.height-1 == map[now.index+K][i]){
                         boolean check = true;
@@ -129,7 +133,6 @@ public class Main {
                     }
                 }
 
-                // 위로 경사로
                 if(now.used != 1){
                     if(now.index + K < N){
                         if(now.height+1 == map[now.index+K][i]){
