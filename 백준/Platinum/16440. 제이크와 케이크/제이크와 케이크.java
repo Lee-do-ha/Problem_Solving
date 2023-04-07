d4641c820f0b272b75c134b65295d27818e1733b
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-07 19:29:48 +0900

    Update 제이크와 케이크.java

--- 백준/Platinum/16440. 제이크와 케이크/제이크와 케이크.java
+++ 백준/Platinum/16440. 제이크와 케이크/제이크와 케이크.java
@@ -15,18 +15,23 @@ public class Main {
 
         char[] fruits = fruit.toCharArray();
 
+        // 절반 길이이므로
         int start = 0;
         int end = (N/2)-1;
 
+        // 처음에는 모두 0개
         int kiwi = 0;
         int straw = 0;
 
+        // 초기 윈도우 값
         for(int i = start ; i <= end ; i++){
             if(fruits[i] == 's') straw++;
             if(fruits[i] == 'k') kiwi++;
         }
 
+        // 무조건 정답이 있으므로 정답이 나올때까지 진행
         while(true){
+            
             if(kiwi == N/4 && straw == N/4){
                 if(start == 0 || end == N-1) {
                     System.out.println(1);
@@ -39,7 +44,7 @@ public class Main {
                     return;
                 }
             }
-
+            // 슬라이딩 윈도우 진행
             if(fruits[start] == 's'){
                 straw--;
                 start++;
