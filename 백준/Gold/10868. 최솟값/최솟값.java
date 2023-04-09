0bfb12d227d9e538db19c843e74e45e9347a845b
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-10 00:57:22 +0900

    Update 최솟값.java

--- 백준/Gold/10868. 최솟값/최솟값.java
+++ 백준/Gold/10868. 최솟값/최솟값.java
@@ -22,6 +22,7 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
+        // 세그먼트트리 초기 세팅
         Init(1, N, 1);
 
         for(int i = 0 ; i < M ; i++){
@@ -34,7 +35,9 @@ public class Main {
         System.out.println(sb);
     }
 
+    // 세그먼트트리 초기 세팅
     private static void Init(int start, int end, int Node){
+        // 시작점과 끝점이 같다면 배열값 가져오기
         if(start == end){
             segTree[Node] = arr[start];
             return ;
@@ -42,24 +45,29 @@ public class Main {
 
         int half = (start + end) / 2;
 
+        // 다시 구간나눠서 트리 값 조정
         Init(start, half, Node*2);
         Init(half+1, end, Node*2+1);
 
         segTree[Node] = Math.min(segTree[Node*2], segTree[Node*2+1]);
     }
 
+    // 트리에서 값 찾기
     private static int find(int start, int end, int Node, int left, int right){
 
+        // 구간을 벗어나는 경우 최솟값을 찾으므로 최대값으로 설정
         if(start > right || end < left){
             return Integer.MAX_VALUE;
         }
 
+        // 구간내에 완전 겹쳐진다면 바로 트리값 가져오기
         if(start >= left && end <= right){
             return segTree[Node];
         }
 
         int half = (start+ end)/2;
 
+        // 다시 트리 나눠서 진행
         return Math.min(find(start, half, Node*2, left, right), find(half+1, end, Node*2+1, left, right));
     }
 }
