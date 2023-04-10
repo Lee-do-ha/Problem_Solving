535169adb75205d51335bd14e67f066c9c018ff6
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-10 22:56:58 +0900

    Update 수열과 쿼리 1.java

--- 백준/Platinum/13537. 수열과 쿼리 1/수열과 쿼리 1.java
+++ 백준/Platinum/13537. 수열과 쿼리 1/수열과 쿼리 1.java
@@ -25,11 +25,8 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 세그먼트트리 세팅
         Init(1, N, 1);
-//        System.out.println();
-//        for(int i = 1 ; i < segTree.length ; i++){
-//            System.out.println(i + " " + segTree[i]);
-//        }
 
         int M = Integer.parseInt(br.readLine());
 
@@ -45,6 +42,7 @@ public class Main {
     }
 
     private static void Init(int start, int end, int node){
+        // start와 end가 같으면 list만들고 해당 인덱스값 넣기
         if(start == end){
             segTree[node] = new ArrayList<>();
             segTree[node].add(arr[start]);
@@ -57,26 +55,32 @@ public class Main {
         Init(half+1, end, node*2+1);
 
 
+        // 양 옆의 값들 추가해주기
         segTree[node] = new ArrayList();
         segTree[node].addAll(segTree[node*2]);
         segTree[node].addAll(segTree[node*2+1]);
+        // 이분탐색위해 정렬
         Collections.sort(segTree[node]);
     }
 
     private static int find(int start, int end, int node, int left, int right, int num){
+        // 범위 벗어난 경우 0개
         if(start > right || end < left){
             return 0;
         }
 
+        // 범위 내에 있는경우 해당 배열의 num이상인 값의 갯수 찾기
         if(left <= start && right >= end && !segTree[node].isEmpty()){
             return binarySearch(segTree[node], num);
         }
 
+        // 구간 나누어서 계속 진행
         int half = (start + end) / 2;
 
         return find(start, half, node*2, left, right, num) + find(half+1, end, node*2+1, left, right, num);
     }
 
+    // 모든 배열 정렬되어있으므로 이분 탐색으로 중간인덱스 찾기
     private static int binarySearch(ArrayList<Integer> List, int num){
         int start = 0;
         int end = List.size()-1;
