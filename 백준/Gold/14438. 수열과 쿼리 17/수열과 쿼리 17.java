092838d5c87049527fa97acc553e337d2b6c0d00
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-13 22:45:11 +0900

    Update 수열과 쿼리 17.java

--- 백준/Gold/14438. 수열과 쿼리 17/수열과 쿼리 17.java
+++ 백준/Gold/14438. 수열과 쿼리 17/수열과 쿼리 17.java
@@ -22,6 +22,7 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 세그먼트트리 초기 세팅
         init(1, N, 1);
 
         int M = Integer.parseInt(br.readLine());
@@ -40,7 +41,9 @@ public class Main {
         System.out.println(sb);
     }
 
+    // 세그먼트 트리 초기 세팅
     private static void init(int start, int end, int node){
+        // 시작과 끝이 같다면 해당 값 가져오기
         if(start == end){
             segTree[node] = arr[start];
             return ;
@@ -48,19 +51,25 @@ public class Main {
 
         int half = (start + end) / 2;
 
+        // 구간 나눠서 진행
         init(start, half, node*2);
         init(half+1, end, node*2+1);
 
+        // 부모 노드는 두 자식 노드 중 최솟값으로 저장
         segTree[node] = Math.min(segTree[node*2], segTree[node*2+1]);
     }
 
+    // 값 바꾸기
     private static void update(int start, int end, int node, int index, int changenum){
 
+        // 바꾸려는 인덱스번호가 찾으려는 구간내에 있을때만 진행
         if(start <= index && index <= end){
             int half = (start + end) / 2;
+            // 해당 인덱스로 도착했다면 값 바꾸기
             if(start == end){
                 segTree[node] = changenum;
                 arr[index] = changenum;
+                // 아니라면 계속 진행하고 돌아오면서 다시 값 갱신하기
             }else{
                 update(start, half, node*2, index, changenum);
                 update(half+1, end, node*2+1, index, changenum);
@@ -69,15 +78,19 @@ public class Main {
         }
     }
 
+    // 값 찾기
     private static int find(int start, int end, int node, int left, int right){
+        // 구간 벗어나면 최대값으로 리턴
         if(start > right || end < left){
             return Integer.MAX_VALUE;
         }
 
+        // 구간내에 완전히 겹친다면 해당 트리값 가져오기
         if(start >= left && end <= right){
             return segTree[node];
         }
 
+        // 구간 나눠서 값 
         int half = (start + end) / 2;
 
         return Math.min(find(start, half, node*2, left, right), find(half+1, end, node*2+1, left, right));
