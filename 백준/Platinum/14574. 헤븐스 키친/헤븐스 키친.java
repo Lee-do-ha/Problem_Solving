ca0ae718867a6a9d72d38bf78d9282e8bdf3016b
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-04-26 23:04:08 +0900

    Update 헤븐스 키친.java

--- 백준/Platinum/14574. 헤븐스 키친/헤븐스 키친.java
+++ 백준/Platinum/14574. 헤븐스 키친/헤븐스 키친.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class Main {
 
+    // 각 요리사 정보 저장할 클래스
     static class cook{
         int p, x;
         public cook(int p, int x) {
@@ -13,6 +14,7 @@ public class Main {
         }
     }
 
+    // MST연결하기위한 시작점, 도착점, 경로값 저장할 클래스
     static class path implements Comparable<path>{
         int start, end;
         long popul;
@@ -23,6 +25,7 @@ public class Main {
             this.popul = popul;
         }
 
+        // 내림차순으로 PriorityQueue 사용하기위함
         @Override
         public int compareTo(path o) {
             if(this.popul > o.popul){
@@ -33,6 +36,7 @@ public class Main {
         }
     }
 
+    // 소속된 집합 표시하기 위한 배열
     static int[] parents;
 
     public static void main(String[] args) throws IOException {
@@ -43,12 +47,15 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         parents = new int[N];
+        // 어느 요리사끼리 연결되어있는지 저장하기위한 리스트
         ArrayList<Integer>[] List = new ArrayList[N];
+        // 몇번 상대해야 하는지 저장할 배열
         int[] used = new int[N];
 
 
         cook[] arr = new cook[N];
 
+        // 초기화하고 생성
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine());
             arr[i] = new cook(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
@@ -56,6 +63,7 @@ public class Main {
             List[i] = new ArrayList<>();
         }
 
+        // 모든 요리사끼리의 대결 정보 PriorityQueue에 저장
         for(int i = 0 ; i < N ; i++){
             for(int j = i + 1 ; j < N ; j++){
                 int x = Math.abs(arr[i].p - arr[j].p);
@@ -64,16 +72,21 @@ public class Main {
             }
         }
 
+        // 연결된 대결 갯수 저장
         int connected = 0;
+        // 결과값 저장
         long result = 0;
         while(!pq.isEmpty()){
             path cur = pq.poll();
+            // 연결안되어있다면 연결 시키고 값 갱신
             if(findSet(cur.start) != findSet(cur.end)){
                 unionSet(cur.start, cur.end);
                 result += cur.popul;
                 connected++;
+                // 양방향으로 연결
                 List[cur.start].add(cur.end);
                 List[cur.end].add(cur.start);
+                // 둘다 대결횟수 추가
                 used[cur.start]++;
                 used[cur.end]++;
 
@@ -85,15 +98,21 @@ public class Main {
 
         sb.append(result).append("\n");
 
+        // 모두 대결이 끝나기 전까지 진행
         while(!tf(used)){
             for(int i = 0 ; i < N ; i++){
+                // 1경기만 남았다면 해당 요리사는 연결되어있는 사람에게 이기고 천국가면 됨
                 if(used[i] == 1){
                     if(!List[i].isEmpty()){
                         for(int k = 0 ; k < List[i].size() ; k++){
+                            // 가지고 있는 상대중에 0보다 크면 대결 진행
                             if(used[List[i].get(k)] > 0 ){
                                 sb.append(List[i].get(k)+1).append(" ").append(i+1).append("\n");
+                                // 대결 횟수 감소
                                 used[i]--;
+                                // 상대의 대결 횟수도 감소
                                 used[List[i].get(k)]--;
+                                // 리스트에서 현재 요리사 제거
                                 List[i].remove(k);
                                 break;
                             }
@@ -107,16 +126,19 @@ public class Main {
 
     }
 
+    // 소속된 집합 찾기
     static int findSet(int a){
         if(a == parents[a]) return a;
 
         return parents[a] = findSet(parents[a]);
     }
 
+    // 소속된 집합 합치기
     static void unionSet(int a, int b){
         parents[findSet(b)] = findSet(a);
     }
 
+    // 모두 종료되었는지 확인
     static boolean tf(int[] arr){
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] != 0) return false;
