7809add847b9804d3df5c156692cbbff8045b609
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2023-06-26 10:57:37 +0900

    [Gold IV] Title: 플로이드, Time: 676 ms, Memory: 45704 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -14,10 +14,8 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
 
-        // 최소 비용 저장할 2차원 배열
         int[][] distance = new int[N][N];
 
-        // 자신을 향하는 경로 빼고는 모두 40000000으로 저장
         for(int i = 0 ; i < N ; i++){
             for(int j = 0 ; j < N ; j++){
                 if(i == j) continue;
@@ -31,27 +29,19 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            // 똑같은 경로가 더 큰값으로 들어올 수 있으므로 값비교해서 더 작은값으로 저장
             distance[a-1][b-1] = Math.min(distance[a-1][b-1], c);
         }
 
-        // 플로이드 진행
-        // 경유지
         for(int k = 0 ; k < N ; k++){
-            // 출발지
             for(int i = 0 ; i < N ; i++){
                 if(k == i) continue;
-                // 목적지
                 for(int j = 0 ; j < N ; j++){
                     if(i == j || k == j) continue;
-                    
-                    // 최소 비용은 거쳐가는 비용과 현재 비용중 더 작은값으로 저장
                     distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
                 }
             }
         }
 
-        // 갱신되지않은 경로면 0으로 저장하고 배열 출력
         for(int i = 0 ; i < N ; i++){
             for(int j = 0 ; j < N ; j++){
                 if(distance[i][j] == 40000000){
