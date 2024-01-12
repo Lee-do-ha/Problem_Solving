8a4a48cfbef25d215c3790c490ee376f6aa34798
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-01-12 17:13:57 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 800 ms, Memory: 57904 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            queue.add(new int[] {x, y});
+        }
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            sb.append(cur[0] + " " + cur[1]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
