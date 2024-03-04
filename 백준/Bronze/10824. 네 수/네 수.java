75385c34ab24955e0ce2cfbffa290af8f754d99e
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-03-04 12:02:47 +0900

    [Bronze III] Title: 네 수, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        String A = st.nextToken();
+        String B = st.nextToken();
+        String C = st.nextToken();
+        String D = st.nextToken();
+
+        System.out.println(Long.parseLong(A + B) + Long.parseLong(C + D));
+    }
+
+}
