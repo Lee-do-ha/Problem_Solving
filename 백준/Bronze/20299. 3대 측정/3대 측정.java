496b58130050ef9607ae30d15f17d3a88b5f6f27
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-03-17 13:58:06 +0900

    [Bronze II] Title: 3대 측정, Time: 928 ms, Memory: 165328 KB -BaekjoonHub

---
+++ 백준/Bronze/20299. 3대 측정/3대 측정.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if(a < L || b < L || c < L) continue;
+
+            if (a + b + c < K) continue;
+
+            ans++;
+            sb.append(a + " " + b + " " + c + " ");
+        }
+        System.out.println(ans);
+        System.out.println(sb);
+
+    }
+
+}
