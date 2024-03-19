113fdd664f30843dc832ea861648f9f50da9b66d
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-03-19 22:02:16 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 144 ms, Memory: 15824 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+
+        sb.append(":fan::fan::fan:").append("\n");
+        sb.append(":fan::" + str + "::fan:").append("\n");
+        sb.append(":fan::fan::fan:");
+
+        System.out.println(sb);
+    }
+
+}
