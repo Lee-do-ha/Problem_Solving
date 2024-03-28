5dac68af71467cfe490d676035b3d4fe192484cf
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-03-28 20:57:48 +0900

    [Silver V] Title: 단어 정렬, Time: 1600 ms, Memory: 25616 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        });
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if(priorityQueue.contains(str)) continue;
+            priorityQueue.add(str);
+        }
+
+        while (!priorityQueue.isEmpty()) {
+            sb.append(priorityQueue.poll()).append("\n");
+        }
+
+        System.out.println(sb);
+        
+    }
+
+}
