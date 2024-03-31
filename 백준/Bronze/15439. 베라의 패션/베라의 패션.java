c18de25dca3d53491c765dd1bb3191ea04e8bb49
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-03-31 18:01:54 +0900

    [Bronze IV] Title: 베라의 패션, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int ans = (int) (Math.pow(N, 2) - N);
+
+        System.out.println(ans);
+
+    }
+
+}
