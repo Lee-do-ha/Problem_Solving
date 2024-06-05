fe03a8b3961cc7c1af9b7729f68e9dc8783945a9
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-06-05 18:24:30 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(N * M - K, 0));
+    }
+
+}
