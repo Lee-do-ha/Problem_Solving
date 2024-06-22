afd9686047a4b26447f49279771238c64ff0355b
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2024-06-22 20:31:30 +0900

    [Bronze V] Title: 새로운 시작, Time: 120 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("파이팅!!");
+    }
+
+}
