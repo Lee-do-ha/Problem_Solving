46f82018d8fd8f95ed785a723a54b26ff8d3936b
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2025-02-27 11:21:20 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
-
-import javax.management.Query;
 
 public class Main {
 	public static void main(String[] args) throws Exception, IOException {
@@ -17,45 +13,28 @@ public class Main {
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		
-		long time = 1;
-		long max = 1000000001;
+		int time = 1;
 		
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		boolean flag = false;
-		boolean[] visited = new boolean[(int) max];
-		
-		queue.add(a);
-		visited[a] = true;
-		
-		while(!flag) {
+		while(b != a) {
 			
-			int size = queue.size();
-			for(int i = 0 ; i < size ; i++) {
-				long cur = queue.poll();
-				
-				if(cur*2 < max && !visited[(int) (cur*2)]) {
-					queue.add((int) (cur*2));
-					visited[(int) (cur*2)] = true;
-				}
-				
-				if(cur*10 + 1 < max && !visited[(int) (cur*10 + 1)]) {
-					queue.add((int) (cur*10 + 1));
-					visited[(int) (cur*10 + 1)] = true;
-				}
+			if (b < a) {
+				System.out.println(-1);
+				return;
+			}
+			
+			if(b%10 == 1) {
+				b /= 10;
+			} else if (b%2 == 0) {
+				b /= 2;
+			} else {
+				System.out.println(-1);
+				return;
 			}
 			
 			time++;
-			if(visited[b]) flag = true;
-			
-			if(flag || queue.isEmpty()) break;
 		}
 		
-		if(!flag) {
-			System.out.println(-1);
-		} else {
-			System.out.println(time);
-		}
+		System.out.println(time);
 	}
 	
 }
