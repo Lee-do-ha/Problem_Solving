1fd643a91eb0d43609457514a57e72cdd0b10781
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2025-03-07 17:50:39 +0900

    [Bronze II] Title: 커트라인, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+        // 점수 저장
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N ; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+        // 점수 오름차순을 정렬
+		Arrays.sort(arr);
+		
+        // 커트라인
+		System.out.println(arr[N-M]);
+	}
+	
+}
