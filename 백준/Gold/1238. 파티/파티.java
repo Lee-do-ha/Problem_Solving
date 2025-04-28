7103b6d2ff399f03e912f026f00104d24e456da9
victoryddh <122585454+Lee-do-ha@users.noreply.github.com>
2025-04-28 13:53:03 +0900

    [Gold III] Title: 파티, Time: 464 ms, Memory: 56948 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	// 도로 저장할 클래스
 	static class path implements Comparable<path>{
 		int end, weight;
 		public path(int end, int weight) {
@@ -22,8 +22,12 @@ public class Main {
 		}
 		
 	}
+	// 도로 저장할 배열
 	static ArrayList<path>[] lists;
+	// 최댓값
 	static int INF = 9999999;
+    // 다익스트라용 PQ
+	static PriorityQueue<path> pq = new PriorityQueue<>();
 	
 	public static void main(String[] args) throws Exception, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,20 +50,25 @@ public class Main {
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			
+			// 단방향이므로 한쪽에만 넣기
 			lists[a].add(new path(b, c));
 		}
 		
+		// 갔다오는 경우이므로 x에서 다익스트라 돌리기
 		int[] dijkstraX = new int[n + 1];
 		dijkstra(x, dijkstraX);
 		
+		// 최종 출력값
 		int ans = 0;
 		
+		// x를 제외헌 모든 지점에서 다익스트라 돌리기
 		int[] dijkstra = new int[n + 1];
 		for(int i = 1 ; i < n + 1 ; i++) {
 			if(i == x) continue;
 			
 			dijkstra(i, dijkstra);
 			
+			// 갔다오는 거리가 가장 긴 거리를 저장
 			ans = Math.max(dijkstra[x] + dijkstraX[i], ans);
 		}
 		
@@ -70,9 +79,7 @@ public class Main {
 	private static void dijkstra(int start, int[] arr) {
 		Arrays.fill(arr, INF);
 		arr[start] = 0;
-		
-		PriorityQueue<path> pq = new PriorityQueue<>();
-		
+				
 		pq.add(new path(start, 0));
 		
 		while(!pq.isEmpty()) {
